0c57a91453f1459a54fb27e8855d65de10b4213c
Akshat Sharma <akshatsharma0023@outlook.com>
2024-07-04 09:31:54 +0530

    writing error messages

---
+++ programs/blink-take-2/src/errors.rs
@@ -0,0 +1,37 @@
+use anchor_lang::prelude::*;
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Market is not active")]
+    MarketNotActive,
+    #[msg("Invalid Bet Choice")]
+    InvalidBetChoice,
+    #[msg("Insufficient Funds")]
+    InsufficientFunds,
+    #[msg("The market is already resolved.")]
+    MarketAlreadyResolved,
+    #[msg("The market is not resolved yet.")]
+    MarketNotResolved,
+    #[msg("Failed to fetch price from Pyth")]
+    PriceFetchFailed,
+    #[msg("Invalid market duration")]
+    InvalidDuration,
+    #[msg("Market has not expired yet")]
+    MarketNotExpired,
+    #[msg("Not a winner")]
+    NotAWinner,
+    #[msg("Initial Price not set")]
+    InitialPriceNotSet,
+    #[msg("Winnings already claimed")]
+    AlreadyClaimed,
+    #[msg("Insufficient funds in the market")]
+    InsufficientMarketFunds,
+    #[msg("Bet amount is too low")]
+    BetAmountTooLow,
+    #[msg("Insufficient user funds")]
+    InsufficientUserFunds,
+    #[msg("Market has already started")]
+    MarketAlreadyStarted,
+    #[msg("Team fee timelock has not expired")]
+    TeamFeeTimelockNotExpired,
+}
