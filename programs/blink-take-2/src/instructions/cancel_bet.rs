4e5d3c6719d52b7ec997a48e02c698c65ee96717
Mukesh <hey.mukesh272921@gmail.com>
2024-07-26 01:48:27 +0530

    fix: change cancellation time (#5)
    * fix: change cancellation time
    
    * fix: fix imports

--- programs/blink-take-2/src/instructions/cancel_bet.rs
+++ programs/blink-take-2/src/instructions/cancel_bet.rs
@@ -3,6 +3,7 @@ use anchor_lang::prelude::*;
 use crate::constants::{USER_POSITION_PDA_SEED, MARKET_PDA_SEED, MARKET_CREATION_AUTHORITY};
 use crate::state::{Market, UserPosition};
 use crate::utils::calculate_refund_amount;
+use crate::errors::ErrorCode;
 
 #[derive(Accounts)]
 pub struct CancelBet<'info> {
@@ -34,14 +35,14 @@ pub fn cancel_bet(ctx: Context<CancelBet>) -> Result<()> {
     let user_position = &mut ctx.accounts.user_position;
 
     let current_time = Clock::get()?.unix_timestamp as u64;
+    let elapsed_time = current_time - market.start_time;
+    let six_hours_in_seconds = 6 * 60 * 60;
 
-    // TODO: ask akshat regarding this - what's the best solution for this such that users can't game the system? commented it for now
-    // if current_time >= market.start_time {
-    //     return Err(ErrorCode::MarketAlreadyStarted.into());
-    // }
+    if elapsed_time <= six_hours_in_seconds {
+        return Err(ErrorCode::MarketAlreadyStarted.into());
+    }
 
     let total_shares = user_position.yes_shares + user_position.no_shares;
-    let elapsed_time = current_time - market.start_time;
     let refund_amount = calculate_refund_amount(total_shares, elapsed_time, market.duration);
 
     market.total_yes_shares -= user_position.yes_shares;
